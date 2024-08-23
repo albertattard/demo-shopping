d7f6e84bade1e7885bce17da742856ba918e6b3c
Albert Attard <albert.attard@oracle.com>
2024-08-23 12:40:12 +0200

    Align example
    Changed the code to be similar to other examples.

--- monolithic-modular/src/test/java/demo/catalogue/CatalogueItemControllerTest.java
+++ monolithic-modular/src/test/java/demo/catalogue/CatalogueItemControllerTest.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class CatalogueItemIT {
+class CatalogueItemControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
